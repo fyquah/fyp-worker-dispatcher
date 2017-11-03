83e82b381ba7964e0ff4f9e297296b8f6f15221e
Fu Yong Quah <quah.fy95@gmail.com>
2017-11-03 19:16:28 +0000

    Pin process to a single core

--- worker/benchmark_binary.sh
+++ worker/benchmark_binary.sh
@@ -9,7 +9,7 @@ function extract_time {
 }
 
 for i in $(seq 1 "$NUM_RUNS"); do
-  TIME_OUTPUT=$((time (cat w/random1M.bin | "$PATH_TO_BINARY" >/dev/null 2>/dev/null)) 2>&1)
+  TIME_OUTPUT=$((time (cat w/random1M.bin | taskset 0x1 "$PATH_TO_BINARY" >/dev/null 2>/dev/null)) 2>&1)
 
   # I don't know why this additional echo is needed -- but since it works i
   # don't question the lord of shell scripts.
