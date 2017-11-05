ba69164a017eae186355b54ab912254e924034eb
Fu Yong Quah <quah.fy95@gmail.com>
2017-11-05 22:02:58 +0000

    Replace subprocess with subshell (less process - more happiness)

--- worker/benchmark_binary.sh
+++ worker/benchmark_binary.sh
@@ -9,10 +9,7 @@ function extract_time {
 }
 
 for i in $(seq 1 "$NUM_RUNS"); do
-  TIME_OUTPUT=$((time (cat w/random1M.bin | taskset 0x1 "$PATH_TO_BINARY" >/dev/null 2>/dev/null)) 2>&1)
-
-  # I don't know why this additional echo is needed -- but since it works i
-  # don't question the lord of shell scripts.
-  echo "$TIME_OUTPUT" | extract_time
+  { time taskset 0x1 "$PATH_TO_BINARY" 1>/dev/null 2>/dev/null; } 2>out_time.txt
+  cat out_time.txt | extract_time
   echo ""
 done
