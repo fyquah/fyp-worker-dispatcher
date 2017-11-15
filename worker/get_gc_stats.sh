194a05b7e32d53655e05ab1b789130a46f10bd0f
Fu Yong Quah <quah.fy95@gmail.com>
2017-11-15 18:26:14 +0000

    m

--- worker/get_gc_stats.sh
+++ worker/get_gc_stats.sh
@@ -1,4 +1,5 @@
 PATH_TO_BINARY="$1"
-ARGS="$2"
+shift
+ARGS="$@"
 
-OCAML_GC_STATS=/dev/stderr ./main.native $ARGS 2>&1 1>/dev/null
+OCAML_GC_STATS=/dev/stderr "$PATH_TO_BINARY" $ARGS 2>&1 1>/dev/null
