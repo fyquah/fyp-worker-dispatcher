06d0dea4d54454c1d216cfaae0eaf96d3eb11cd5
Fu Yong Quah <quah.fy95@gmail.com>
2017-11-17 22:24:09 +0000

    Make plots more readable

--- plots/simulated_annealing_plot.py
+++ plots/simulated_annealing_plot.py
@@ -2,6 +2,7 @@ import argparse
 import sys
 import csv
 
+import matplotlib
 from matplotlib import pylab as plt
 from matplotlib.backends.backend_pdf import PdfPages
 import pylab
@@ -10,7 +11,7 @@ import numpy as np
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--title", type=str, dest="title")
-parser.add_argument("--pdf", type=str, dest="pdf", default=None)
+parser.add_argument("--output", type=str, dest="output", default=None)
 
 def main():
     args = parser.parse_args()
@@ -19,11 +20,20 @@ def main():
         arr.append([float(x) for x in line])
     arr = np.array(arr)
 
+    plt.rc("font", family="serif", serif=["Computer Modern"])
+    plt.rc("xtick", labelsize=8)
+    plt.rc("ytick", labelsize=8)
+    plt.rc("axes", labelsize=8)
+    # plt.rc("lines", linewidth=1.0, marker="x", markersize=3)
+    plt.rc("lines", linewidth=1.0)
+
+    fig, ax = plt.subplots()
     plt.suptitle(args.title)
 
     plt.subplot(2, 2, 1)
     l1, = plt.plot(arr[:, 0], arr[:, 1], label="current")
     l2, = plt.plot(arr[:, 0], arr[:, 2], label="proposal")
+    plt.grid()
     plt.title("Iteration vs Execution time")
     plt.xlabel("iteration")
     plt.ylabel("execution time")
@@ -32,6 +42,7 @@ def main():
     plt.subplot(2, 2, 2)
     l1, = plt.plot(arr[:, 0], arr[:, 3], label="current")
     l2, = plt.plot(arr[:, 0], arr[:, 4], label="proposal")
+    plt.grid()
     plt.title("Iteration vs Energy")
     plt.xlabel("iteration")
     plt.ylabel("energy (runtime relative to original)")
@@ -40,6 +51,7 @@ def main():
     plt.subplot(2, 2, 3)
     l1, = plt.plot(arr[:, 0], arr[:, 5], label="current")
     l2, = plt.plot(arr[:, 0], arr[:, 6], label="proposal")
+    plt.grid()
     plt.title("Iteration vs Major collections")
     plt.xlabel("iteration")
     plt.ylabel("major collections")
@@ -48,18 +60,18 @@ def main():
     plt.subplot(2, 2, 4)
     l1, = plt.plot(arr[:, 0], arr[:, 7], label="current")
     l2, = plt.plot(arr[:, 0], arr[:, 8], label="proposal")
+    plt.grid()
     plt.title("Iteration vs Minor collections")
     plt.xlabel("iteration")
     plt.ylabel("minor collections")
     plt.legend(handles=[l1, l2], loc="best")
 
-    if not args.pdf:
+    fig.set_size_inches(10, 10)
+
+    if not args.output:
         plt.show()
     else:
-        pp = PdfPages(args.pdf)
-        plt.savefig(pp, format="pdf")
-        pp.savefig()
-        pp.close()
+        plt.savefig(args.output)
 
 
 if __name__ == "__main__":
