72989871d622250a6b4fb392385da73bcdf07f08
Fu Yong Quah <quah.fy95@gmail.com>
2018-02-09 09:58:49 +0000

    Use Minitor.protect to manage locks

--- controller/with_lock.ml
+++ controller/with_lock.ml
@@ -10,14 +10,14 @@ let run ?(name="Anon") lock utilise_resource =
       | Error _ ->
         Log.Global.debug "%s xailed to acquire lock" name;
         Clock.after (Time.Span.of_sec 1.0) >>| fun () -> (`Repeat ()))
-  >>= fun () -> utilise_resource ()
-  >>= fun ret ->
-  Deferred.repeat_until_finished () (fun () ->
-    match Nano_mutex.unlock lock with
-    | Ok () ->
-      Log.Global.info "%s released lock" name;
-      Deferred.return (`Finished ())
-    | Error _ ->
-      Log.Global.debug "%s failed to release lock" name;
-      Clock.after (Time.Span.of_sec 1.0) >>| fun () -> `Repeat ())
-  >>| fun () -> ret
+  >>= fun () ->
+  Monitor.protect utilise_resource ~finally:(fun () -> 
+    Deferred.repeat_until_finished () (fun () ->
+      match Nano_mutex.unlock lock with
+      | Ok () ->
+        Log.Global.info "%s released lock" name;
+        Deferred.return (`Finished ())
+      | Error _ ->
+        Log.Global.debug "%s failed to release lock" name;
+        Clock.after (Time.Span.of_sec 1.0) >>| fun () -> `Repeat ()))
+;;
