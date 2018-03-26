cfb381a6c1027cb486d322210441656f2e6c2689
Fu Yong Quah <quah.fy95@gmail.com>
2018-03-26 21:41:34 +0100

    Add benefit function to hyperparameters

--- analysis/learn_linear_general_reward.py
+++ analysis/learn_linear_general_reward.py
@@ -18,15 +18,17 @@ parser = argparse.ArgumentParser(description="formulate the problem")
 parser.add_argument("directory", type=str, help="experiment dir")
 parser.add_argument("--decay-factor", type=float, default=None, required=True)
 parser.add_argument("--ridge-factor", type=float, default=None, required=True)
+parser.add_argument("--benefit-function", type=str, default=None, required=True)
 
 
 HyperParametersBase = collections.namedtuple("HyperParametersBase",
-        ["decay_factor", "ridge_factor"])
+        ["decay_factor", "ridge_factor", "benefit_function"])
 
 class HyperParameters(HyperParametersBase):
 
     def directory_name(self):
-        return "decay-%f-ridge-%f" % (self.decay_factor, self.ridge_factor)
+        return "decay-%f-ridge-%f-benefit-%s" % (
+                self.decay_factor, self.ridge_factor, self.benefit_function)
 
 def is_very_small(x):
     return x < 1e-10
@@ -67,10 +69,29 @@ def compute_vector_space_of_solutions(R, rhs):
     print(w)
 
 
-def construct_benefit_from_exec_time(execution_times):
+def _sigmoid_speedup_over_mean(execution_times):
     time_average = np.mean(execution_times)
     return learn_problem.sigmoid(
             -20 * (execution_times - time_average) / time_average)
+
+
+def _linear_speedup_over_mean(execution_times):
+    time_average = np.mean(execution_times)
+    return -(execution_times - time_average) / time_average
+
+
+def _log_speedup_over_mean(execution_times):
+    time_average = np.mean(execution_times)
+    return -np.log(execution_times / time_average)
+
+
+def construct_benefit_from_exec_time(kind, execution_times):
+    dispatch = {
+            "sigmoid_speedup_over_mean": _sigmoid_speedup_over_mean,
+            "linear_speedup_over_mean":  _linear_speedup_over_mean,
+            "log_speedup_over_mean":     _log_speedup_over_mean,
+    }
+    return dispatch[kind](execution_times)
 
 
 def main():
@@ -83,7 +104,8 @@ def main():
     logging.info("Loading problem definition from disk")
     hyperparams = HyperParameters(
             decay_factor=args.decay_factor,
-            ridge_factor=args.ridge_factor)
+            ridge_factor=args.ridge_factor,
+            benefit_function=args.benefit_function)
     exp_directory = os.path.join(
             problem_directory, EXPERIMENT_NAME, hyperparams.directory_name())
     if not os.path.exists(exp_directory):
@@ -95,19 +117,21 @@ def main():
     problem_matrices = learn_problem.construct_problem_matrices(
             problem, hyperparams)
 
-    target_benefit = construct_benefit_from_exec_time(execution_times)
+    target_benefit = construct_benefit_from_exec_time(
+            args.benefit_function, execution_times)
     num_features = problem_matrices.benefit_relations.shape[1]
 
     logging.info("Computing analytical solution.")
     logging.info("  decay factor = %.6f" % (args.decay_factor))
     logging.info("  ridge factor (aka l2 reg) = %.6f" % (args.ridge_factor))
+    logging.info("  benefit function = %s" % args.benefit_function)
     lambda_ = args.ridge_factor
     A = problem_matrices.benefit_relations
     w = np.linalg.solve(
             np.matmul(A.T , A) + (lambda_ * np.identity(A.shape[1])),
             np.matmul(A.T, target_benefit)
     )
-    logging.info("Found analytical solution!")
+    logging.info("Found analytical solution, saving to %s!" % exp_directory)
     with open(os.path.join(exp_directory, "contributions.npy"), "wb") as f:
         np.save(f, w)
 
--- analysis/learn_linear_general_reward.sh
+++ analysis/learn_linear_general_reward.sh
@@ -3,20 +3,24 @@ set -euo pipefail
 BENCHMARK_OUT_DIR=$1
 
 ## Thorough parameters
-DECAY_FACTORS="0.3 0.5 0.9 0.93 0.95 0.99 0.993 0.995 0.999"
-RIDGE_FACTORS="0.003 0.005 0.009 0.03 0.05 0.09 0.3 0.5 0.9"
+# DECAY_FACTORS="0.3 0.5 0.9 0.93 0.95 0.99 0.993 0.995 0.999"
+# RIDGE_FACTORS="0.003 0.005 0.009 0.03 0.05 0.09 0.3 0.5 0.9"
 
 ## Simple parameters
-# DECAY_FACTORS="0.5 0.9 0.95 0.99 0.999"
-# RIDGE_FACTORS="0.03 0.07 0.3 0.7"
+DECAY_FACTORS="0.3 0.7 0.93 0.97 0.993 0.997"
+RIDGE_FACTORS="0.003 0.007 0.03 0.07 0.3 0.7"
+BENEFIT_FUNCTIONS="sigmoid_speedup_over_mean linear_speedup_over_mean log_speedup_over_mean"
 
 # Hyperparameter sweep
-for decay_factor in $DECAY_FACTORS; do
-  for ridge_factor in $RIDGE_FACTORS; do
-    python learn_linear_general_reward.py  \
-      --decay-factor $decay_factor \
-      --ridge-factor $ridge_factor \
-      "$BENCHMARK_OUT_DIR"
+for benefit_function in $BENEFIT_FUNCTIONS; do
+  for decay_factor in $DECAY_FACTORS; do
+    for ridge_factor in $RIDGE_FACTORS; do
+      python learn_linear_general_reward.py  \
+        --decay-factor $decay_factor \
+        --ridge-factor $ridge_factor \
+        --benefit-function $benefit_function \
+        "$BENCHMARK_OUT_DIR"
+    done
   done
 done
 
