ff693a139daf963b977d283df73c59135160a488
Fu Yong Quah <fyq14@ic.ac.uk>
2018-04-03 17:45:31 +0100

    Plot additional things on top of main things

--- analysis/plot_model_general_performance.py
+++ analysis/plot_model_general_performance.py
@@ -1,5 +1,8 @@
-import os
+import argparse
+import collections
 import csv
+import os
+import re
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -22,9 +25,31 @@ def geometric_mean(times):
         acc *= t
     return acc ** (1.0 / len(times))
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--decay-factor", type=str, help="filename")
+parser.add_argument("--ridge-factor", type=str, help="filename")
+parser.add_argument("--benefit-function", type=str, help="")
+
+def almost_equal(a, b):
+    return np.abs(float(a) - float(b)) < 0.000001
+
+def matches_filter(filename, args):
+    m = re.search("decay-([\.0-9]+)-ridge-([\.0-9]+)-benefit-([a-zA-Z_]+)", filename)
+    decay_factor = m.group(1)
+    ridge_factor = m.group(2)
+    benefit_function = m.group(3)
+    assert decay_factor is not None 
+    assert ridge_factor is not None 
+    assert benefit_function is not None 
+    return ((args.decay_factor is None or almost_equal(decay_factor, args.decay_factor))
+            and (args.ridge_factor is None or almost_equal(ridge_factor, args.ridge_factor))
+            and (args.benefit_function is None or benefit_function == args.benefit_function))
 
 def main():
+    args = parser.parse_args()
     plot_data = []
+    initial_exec_time_by_bench = {}
+    best_times_by_bench = {}
     for benchmark in py_common.EXPERIMENT_TO_PARAMETERS.keys():
         initial_exec_times = []
         best_time = None
@@ -37,13 +62,18 @@ def main():
                     best_time = t
                 else:
                     best_time = min(best_time, t)
-        initial_exec_time = geometric_mean(initial_exec_times)
+        initial_exec_time_by_bench[benchmark] = geometric_mean(initial_exec_times)
+        best_times_by_bench[benchmark] = best_time
+    del best_time
+    all_records = collections.defaultdict(list)
 
+    for benchmark in py_common.EXPERIMENT_TO_PARAMETERS.keys():
         bench_dir = (
             "../results/%s/linear-general-reward-without-normalisation/"
             % benchmark)
         csv_files = os.listdir(bench_dir)
-        proposed_time = None
+        initial_exec_time = initial_exec_time_by_bench[benchmark]
+        best_time = best_times_by_bench[benchmark]
         for filename in csv_files:
             with open(os.path.join(bench_dir, filename), "rb") as f:
                 times = []
@@ -51,19 +81,42 @@ def main():
                     times.append(parse_time(line[4]))
                 if len(times) >= 1:
                     time = geometric_mean(times)
-                    if proposed_time is None:
-                        proposed_time = time
-                    else:
-                        proposed_time = min(proposed_time, time)
-        if proposed_time is not None:
-            plot_data.append((benchmark, (proposed_time - best_time) / (initial_exec_time - best_time)))
-        print benchmark, (proposed_time - best_time) / (initial_exec_time - best_time), proposed_time, initial_exec_time, best_time
+                    ratio = (time - best_time) / (initial_exec_time - best_time)
+                    all_records[benchmark].append((filename, time, ratio))
+
+
+    for benchmark in py_common.EXPERIMENT_TO_PARAMETERS.keys():
+        best_ratio = None
+        worst_ratio = None
+        active_ratio = None
+
+        for filename, _time, ratio in all_records[benchmark]:
+            if best_ratio is None:
+                best_ratio = ratio
+                worst_ratio = ratio
+            else:
+                best_ratio = min(best_ratio, ratio)
+                worst_ratio = max(worst_ratio, ratio)
+
+            if matches_filter(filename, args):
+                if active_ratio is None:
+                    active_ratio = ratio
+                else:
+                    active_ratio = min(active_ratio, ratio)
+
+        plot_data.append((benchmark, best_ratio, worst_ratio, active_ratio))
+        print benchmark, best_ratio, initial_exec_time, best_time
+
     x = [a[0] for a in plot_data]
-    y = [a[1] for a in plot_data]
+
     plt.title("Individually-tuned Models on Individual Benchmarks")
     plt.axhline(y=1.0, color='r', linestyle='--')
     plt.axhline(y=0.0, color='g', linestyle='--')
-    plt.bar(x, y)
+
+    plt.plot(x, [a[1] for a in plot_data], 'gx')
+    plt.plot(x, [a[2] for a in plot_data], 'rx')
+    plt.bar(x, [a[3] or 0 for a in plot_data])
+
     plt.show()
 
 if __name__ == "__main__":
