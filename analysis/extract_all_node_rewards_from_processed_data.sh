51351c2899514b8ac2d5db6184dd5353fe053961
Fu Yong Quah <quah.fy95@gmail.com>
2018-04-07 03:56:43 +0100

    Log time results

--- analysis/extract_all_node_rewards_from_processed_data.sh
+++ analysis/extract_all_node_rewards_from_processed_data.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for exp in $(python ../scripts/query_experiment_params.py --all); do
-  echo "time python extract_node_rewards_from_processed_data.py --output-dir out/$exp --experiment-name $exp &>tmp/extract-$exp.log"
+  echo "(time python extract_node_rewards_from_processed_data.py --output-dir out/$exp --experiment-name $exp) &>tmp/extract-$exp.log"
 done | parallel $PARALLEL_FLAGS
