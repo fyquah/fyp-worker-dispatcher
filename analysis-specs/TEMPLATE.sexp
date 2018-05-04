490d735b8a535170ea9b020dfe3e0f4b010fc6bb
Fu Yong Quah <quah.fy95@gmail.com>
2018-05-04 17:09:50 +0100

    Add more analysis spec generators

--- analysis-specs/TEMPLATE.sexp
+++ analysis-specs/TEMPLATE.sexp
@@ -1,5 +1,3 @@
-(Unsegmented
-  (((name EXP)
-    (features_file w/EXP/features.bin)
-    (rewards_file w/EXP/rewards.sexp))
-  ))
+(((name EXP)
+  (features_file w/EXP/features.bin)
+  (rewards_file w/EXP/rewards.sexp)))
--- analysis-specs/gen_all.sh
+++ analysis-specs/gen_all.sh
@@ -3,11 +3,3 @@ cd $(dirname $0)
 for exp in $(python ../scripts/query_experiment_params.py --all); do
   sed TEMPLATE.sexp -e "s/EXP/$exp/g" >./$exp.sexp
 done
-
-echo "(" >ALL.sexp
-for exp in $(python ../scripts/query_experiment_params.py --all); do
-  echo " ((name $exp)" >>ALL.sexp
-  echo "  (features_file w/$exp/features.bin)" >>ALL.sexp
-  echo "  (rewards_file w/$exp/rewards.sexp))" >>ALL.sexp
-done
-echo ")" >>ALL.sexp
