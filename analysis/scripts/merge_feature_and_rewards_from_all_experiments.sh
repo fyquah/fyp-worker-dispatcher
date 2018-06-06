36ac7de5be7254c0a6cbf77925971519be465c0a
Fu Yong Quah <quah.fy95@gmail.com>
2018-06-06 09:20:06 +0100

    fix feature-reward fump

--- analysis/scripts/merge_feature_and_rewards_from_all_experiments.sh
+++ analysis/scripts/merge_feature_and_rewards_from_all_experiments.sh
@@ -5,16 +5,26 @@ set -x
 
 cd $(dirname $0)/../../
 
-echo "VERSION = $VERSION"
+echo "VERSION = $REWARD_VERSION"
 TMP=$(mktemp)
 
 for exp in $(python scripts/query_experiment_params.py --all-old); do
   echo "(Unsegmented (
     ((name $exp)
      (features_file w/$exp)
-     (rewards_file w/$VERSION/$exp/rewards.sexp))))" > $TMP
+     (rewards_file w/$REWARD_VERSION/$exp/rewards.sexp))))" > $TMP
+
+  ./_build/default/tools/local_reward_model.exe plots dump-v1-data \
+    -feature-version V1 \
+    -spec $TMP >w/$REWARD_VERSION/$exp/feature_reward_pair.sexp
 
   ./_build/default/tools/local_reward_model.exe plots dump-rewards \
-    -feature-version V1 \
-    -spec $TMP >w/$VERSION/$exp/feature_reward_pair.sexp
+    w/$REWARD_VERSION/$exp/rewards.sexp \
+    >w/$REWARD_VERSION/$exp/rewards_dump.sexp \
+
+  cp w/$REWARD_VERSION/$exp/feature_reward_pair.sexp \
+    ./analysis/report_plots/reward_assignment/data/$exp/
+
+  cp w/$REWARD_VERSION/$exp/rewards_dump.sexp \
+    ./analysis/report_plots/reward_assignment/data/$exp/
 done
