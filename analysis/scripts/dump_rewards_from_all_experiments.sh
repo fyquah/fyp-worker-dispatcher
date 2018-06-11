19e212647f52eb55e37fce0afcb8312ca12c3f92
Fu Yong Quah <quah.fy95@gmail.com>
2018-06-11 17:21:13 +0100

    Results shit

--- analysis/scripts/dump_rewards_from_all_experiments.sh
+++ analysis/scripts/dump_rewards_from_all_experiments.sh
@@ -6,11 +6,14 @@ echo "VERSION = $VERSION"
 
 cd $(dirname $0)/..
 
+set -x
+
 for exp in $(python ../scripts/query_experiment_params.py --all-old); do
-  mkdir -p ../w/$VERSION/$exp
+  echo "dumping to $(realpath ../w/reward-$VERSION/$exp/rewards.sexp)"
+  mkdir -p ../w/reward-$VERSION/$exp
   python ./print_linear_general_reward.py \
-    --experiment-dir out-$VERSION/$exp/linear-general-reward-without-normalisation/decay-1.000000-ridge-0.050000-benefit-tanh_speedup_over_mean \
+    --experiment-dir out-$VERSION/$exp/linear-general-reward-without-normalisation/$(cat out-$VERSION/best-hyperparams.sexp | sexp-query  "(field $exp)") \
     --problem-dir out-$VERSION/$exp/ \
-    --dump-rewards >../w/$VERSION/$exp/rewards.sexp
-    # --experiment-dir out-$VERSION/$exp/linear-general-reward-without-normalisation/$(cat out-$VERSION/best-hyperparams.sexp | sexp-query  "(field $exp)") \
+    --dump-rewards >../w/reward-$VERSION/$exp/rewards.sexp
+    # --experiment-dir out-$VERSION/$exp/linear-general-reward-without-normalisation/decay-1.000000-ridge-0.050000-benefit-tanh_speedup_over_mean \
 done
