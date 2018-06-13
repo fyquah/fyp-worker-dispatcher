aa08795a71d45c955311241404805df00c7eddb3
Fu Yong Quah <fyq14@ic.ac.uk>
2018-06-14 00:58:26 +0100

    Remove symbolic-maths (again)

--- scripts/py_common.py
+++ scripts/py_common.py
@@ -149,13 +149,6 @@ EXPERIMENT_TO_PARAMETERS = {
             bin_args="400000000",
             module_paths=["main"]
         ),
-
-        "fyq-symbolic-maths": ExperimentParameters(
-            bin_name="main",
-            subdir="fyq/symbolic-maths",
-            bin_args="44",
-            module_paths=["main"]
-        ),
 }
 
 INITIAL_EXPERIMENTS = [
