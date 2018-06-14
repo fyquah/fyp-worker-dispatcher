36708b74e4a43f085a3eaf53dcca0f4113f35fb1
Fu Yong Quah <fyq14@ic.ac.uk>
2018-06-15 00:31:46 +0100

    Bulk benching

---
+++ tools/fyp_compiler_plugins/bulk_bench.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -euo pipefail
+
+./compile_and_benchmark_v1_lasso.sh hand   
+./compile_and_benchmark_v1_lasso.sh general
+./compile_and_benchmark_v1_lasso.sh star   
+
+BOUNDS="0.00005 0.0001 0.0005 0.001 0.005 0.05 0.01"
+
+for b in $BOUNDS; do
+   ./compile_and_benchmark_v1_ridge.sh hand    $b
+   ./compile_and_benchmark_v1_ridge.sh general $b
+   ./compile_and_benchmark_v1_ridge.sh star    $b
+done
