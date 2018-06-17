fd273ccf8ad8add61bf6382e5def08e43be88cd1
Fu Yong Quah <fyq14@ic.ac.uk>
2018-06-17 21:20:27 +0100

    Comment out plugin stuff

--- setup_eval_deps.sh
+++ setup_eval_deps.sh
@@ -8,8 +8,6 @@ export OPAMROOT="$HOME/fyp/opam-root"
 eval `opam config env`
 echo `which ocamlopt`
 
-PREFIX=/home/fyquah/dev/machine-learning/inliner/worker-dispatcher/tools/fyp_compiler_plugins/build/plugin_v1_neural_network
-
 echo "Installing benchmark dependencies"
 export OCAMLPARAM="_,O3=1"
 opam install -y depext jbuilder conf-gmp zarith easy-format biniou async lwt cohttp-lwt cohttp-lwt-unix psmt2-frontend camlzip ocplib-simplex react async uuidm ocplib-endian camlimages
