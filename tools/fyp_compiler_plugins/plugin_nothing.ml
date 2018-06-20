f89eb30a7bf62eb5411fa42ebf8feacd573f3573
Fu Yong Quah <fyq14@ic.ac.uk>
2018-06-20 03:15:47 +0100

    Log the key clflags in plugin_nothing

--- tools/fyp_compiler_plugins/plugin_nothing.ml
+++ tools/fyp_compiler_plugins/plugin_nothing.ml
@@ -1,1 +1,19 @@
-let () = ()
+let () =
+  Printf.eprintf "Compilation flags:\n";
+  Printf.eprintf "- unbox_closures = %b\n" (!Clflags.unbox_closures);
+
+  for i = 0 to 2 do
+    Printf.eprintf "- inline_max_unroll[%d] = %d\n" i
+      (Clflags.Int_arg_helper.get ~key:i !Clflags.inline_max_unroll)
+  done;
+
+  for i = 0 to 2 do
+    Printf.eprintf "- inline_max_depth[%d] = %d\n" i
+      (Clflags.Int_arg_helper.get ~key:i !Clflags.inline_max_depth)
+  done;
+
+  for i = 0 to 2 do
+    Printf.eprintf "- inline_threshold[%d] = %.3f\n" i
+        (Clflags.Float_arg_helper.get ~key:i !Clflags.inline_threshold)
+  done
+;;
